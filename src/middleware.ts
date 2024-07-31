334fb886924f762c2fd6b1f10aa7f42aefc2edd6
AkashKumar-0 <ahmvaad@gmail.com>
2024-07-31 22:21:20 +0530

    middleware updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,19 +3,10 @@ import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   const token = request.cookies.get("token");
-  const { pathname } = request.nextUrl;
-  console.log("checking token", token);
 
-  // If the user is not logged in and trying to access a protected route
-  if (!token && pathname !== "/signin" && pathname !== "/signup") {
-    return NextResponse.redirect(new URL("/signin", request.url));
+  if (!token) {
+    return NextResponse.rewrite(new URL("/check-token", request.url));
   }
-
-  // If the user is logged in and trying to access signin or signup pages
-  if (token && (pathname === "/signin" || pathname === "/signup")) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
-
   return NextResponse.next();
 }
 
