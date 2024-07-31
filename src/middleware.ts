25450830ce479e7fcf3b6cf83a166acc6210e58b
AkashKumar-0 <ahmvaad@gmail.com>
2024-07-31 23:12:17 +0530

    psuh

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get("token");
+  const token = request.cookies.get("token")?.value;
 
   if (!token) {
     return NextResponse.rewrite(new URL("/check-token", request.url));
